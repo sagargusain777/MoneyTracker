48e92550eb84fe6c689f84ed5dcd4bc210f5d648
uwuxverticuse <sagargusain150@gmail.com>
2024-03-01 17:54:10 +0530

    Added to Css

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,39 @@ function App() {
       </form>
 
       <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className="title">+200 Samsung Tv</div>
+            <div className="description">Time For New Samsung Tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$200.00</div>
+            <div className="datetime">2024-02-06 15:65</div>
+          </div>
+        </div>
 
+        <div className="transaction">
+          <div className="left">
+            <div className="title">+200 Samsung Tv</div>
+            <div className="description">Time For New Samsung Tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$200.00</div>
+            <div className="datetime">2024-02-06 15:65</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className="title">+200 Samsung Tv</div>
+            <div className="description">Time For New Samsung Tv</div>
+          </div>
+          <div className="right">
+            <div className="price">$200.00</div>
+            <div className="datetime">2024-02-06 15:65</div>
+          </div>
+        </div>
       </div>
-
     </main>
   );
 }
